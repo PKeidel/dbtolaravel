a621900f6159245d3d25f3504b473f7d6542bbb6
Philipp Keidel <pkeidel@sags-per-mail.de>
2017-09-07 14:57:34 +0200

    Travis and PHPUnit

---
+++ tests/FirstTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace PKeidel\DBtoLaravel\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class FirstPHPUnitTest extends TestCase
+{
+    public function testPhpunit()
+    {
+        $this->assertTrue(true);
+    }
+
+}
